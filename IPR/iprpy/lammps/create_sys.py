2d5d5f13c7a26dfbe16e81b6654f5a446638a221
Lucas Hale <lucas.hale@nist.gov>
2015-11-25 10:52:10 -0500

    Updated dislocation tool Notebooks

--- IPR/iprpy/lammps/create_sys.py
+++ IPR/iprpy/lammps/create_sys.py
@@ -3,12 +3,10 @@ import subprocess
 
 import numpy as np
 
-import iprpy
 from read_dump import read_dump
-from sys_gen import sys_gen
 
 #Uses LAMMPS to create a iprpy.System from the supplied system parameters    
-def create_sys(system_info):
+def create_sys(lammps_exe, system_info):
 
     newline = '\n'
     script = newline.join([system_info,
@@ -17,7 +15,7 @@ def create_sys(system_info):
                            'pair_style none',
                            'atom_modify sort 0 0.0',
                            '',                           
-                           'dump dumpit all custom 100000 temp.dump id type x y z'
+                           'dump dumpit all custom 100000 temp.dump id type x y z',
                            'dump_modify dumpit format "%i %i %.13e %.13e %.13e"',                           
                            'run 0'])
     f = open('create_sys.in', 'w')
@@ -25,8 +23,8 @@ def create_sys(system_info):
     f.close()
     output = subprocess.check_output(lammps_exe + ' -in create_sys.in', shell=True)
 
-    sys0 = read_dump(file_name)
+    system = read_dump('temp.dump')
     os.remove('create_sys.in')
-    os.remove('log.lammps')
+    #os.remove('log.lammps')
     os.remove('temp.dump')
-    return sys0      
+    return system      
