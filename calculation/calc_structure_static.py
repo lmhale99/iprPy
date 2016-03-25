de8e5b1ce14241caf38db1e71a1eae57d2a7debd
Lucas Hale <lucas.hale@nist.gov>
2016-03-25 11:47:33 -0400

    updated calc_structure_static

--- calculation/calc_structure_static.py
+++ calculation/calc_structure_static.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 import atomman as am
 import atomman.lammps as lmp
 import atomman.unitconvert as uc
--- process.py
+++ process.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 import process
 
 def main():
     
-    xml_dir = 'C:/users/lmh1/Documents/iprPy_run/xml_library' 
+    xml_dir = 'C:/users/lmh1/Documents/iprPy_run/xml_library2' 
     
     process.structure_static(xml_dir)
     
