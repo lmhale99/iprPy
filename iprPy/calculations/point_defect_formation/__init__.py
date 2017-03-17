0c15d8df28e871deea9beb3178c5f7217605368a
Lucas Hale <lucas.hale@nist.gov>
2017-03-17 14:27:09 -0400

    Updated calc_point_defect_formation

--- iprPy/calculations/point_defect_formation/__init__.py
+++ iprPy/calculations/point_defect_formation/__init__.py
@@ -1,8 +1,20 @@
-raise ValueError('NEEDS REIMPLEMENTING!')
 import os
-from prepare import prepare
-from data_model import data_model
-from read_input import read_input
+from calc_point_defect_formation import data_model, read_input
+
+__calc_dir__ = os.path.dirname(os.path.abspath(__file__))
+__calc_type__ = os.path.basename(__calc_dir__)
+__calc_name__ = 'calc_' + __calc_type__
+
+def template():
+    with open(os.path.join(__calc_dir__, __calc_name__+'.template')) as template_file:
+        return template_file.read()
+
+def files():
+    file_names = [__calc_name__+'.py',
+                  'min.template']
+                      
+    for i in xrange(len(file_names)):
+        yield os.path.join(__calc_dir__, file_names[i])
 
 
 
