abc9ce48490a840b8ca3f5474886d8dcafb2598d
Lucas Hale <lucas.hale@nist.gov>
2017-06-01 12:20:17 -0400

    First demo Notebook

--- iprPy/tools/__init__.py
+++ iprPy/tools/__init__.py
@@ -2,6 +2,5 @@ from .aslist import aslist, iaslist
 from .filltemplate import filltemplate
 from .parseinput import parseinput
 from .termtodict import termtodict
-from .test_lammps_stress_version import test_lammps_stress_version
 from .screen_input import screen_input
 from . import lammps_version
