5d34468da3be2c4c71d57d590bf307a99c258099
Lucas Hale <lucas.hale@nist.gov>
2017-10-10 09:55:05 -0400

    added match_df listing all matching records

--- iprPy/record_functions.py
+++ iprPy/record_functions.py
@@ -323,3 +323,118 @@ class Record(object):
         
         # Return False indicating that there are matches
         return False
+        
+    def match_df(self, record_df=None, record_list=None, database=None,
+              terms=None, fterms=None, atol=0.0, rtol=1e-8):
+        """
+        Checks the record versus a database, list of records, or DataFrame of
+        records to see if any records exists with matching terms and fterms.
+        
+        Parameters
+        ----------
+        record_df : pandas.DataFrame, optional
+            DataFrame to compare record againts.  record_df must be built by
+            converting records to dictionaries using Record.todict(full=False,
+            flat=True), then converting the list of dictionaries to a
+            DataFrame.  Either record_df, record_list or database must be given.
+        record_list : list of iprPy.Records, optional
+            List of Records to compare against.  Either record_df, record_list
+            or database must be given.
+        database : iprPy.Database, optional
+            Database containing records of record.style to compare against.
+            All records of record.style contained in the database will be
+            checked.  Either record_df, record_list or database must be given.
+        terms : list of str, optional
+            The keys of the dictionary produced by Record.todict(full=False,
+            flat=True) to check for equivalency, i.e. use == comparisons for
+            terms with str and int values. If not given, will use the record
+            style's compare_terms.
+        fterms : list of str, optional
+            The keys of the dictionary produced by Record.todict(full=False,
+            flat=True) to check for approximately equal values, i.e. use
+            numpy.isclose() for terms with float values. If not given, will
+            use the record style's compare_fterms.
+        atol : float, optional
+            The absolute tolerance to use in numpy.isclose() for comparing
+            fterms (Default value is 0.0).
+        rtol : float, optional
+            The relative tolerance to use in numpy.isclose() for comparing
+            fterms (Default value is 1e-8).
+        
+        Returns
+        -------
+        bool
+        
+        Raises
+        ------
+        ValueError
+            If more than one of record_df, record_list, and database are 
+            given.
+        """
+        
+        # Convert database to record_list
+        if database is not None:
+            if record_df is not None:
+                raise ValueError('record_df and database cannot both be provided')
+            if record_list is not None:
+                raise ValueError('record_list and database cannot both be provided')
+            
+            record_list = database.get_records(style=self.style)
+        
+        # Convert record_list to record_df
+        if record_list is not None:
+            if record_df is not None:
+                raise ValueError('record_df and record_list cannot both be provided')
+            
+            record_df = []
+            for r in record_list:
+                record_df.append(r.todict(full=False, flat=True))
+            record_df = pd.DataFrame(record_df)
+            
+        # Return True if no records in record_df
+        if len(record_df) == 0:
+            return True
+        
+        # Get default terms and fterms lists
+        if terms is None:
+            terms = self.compare_terms
+        if fterms is None:
+            fterms = self.compare_fterms
+        
+        # Convert record to dictionary
+        record_dict = self.todict(full=False, flat=True)
+
+        # Define slice for comparison testing
+        test_df = record_df
+        
+        # Compare string and int terms
+        for term in terms:
+            # Compare total size multipliers (high-low)
+            if   term == 'a_mult':
+                test_df = test_df[test_df['a_mult2']-test_df['a_mult1'] == record_dict['a_mult2']-record_dict['a_mult1']]
+            elif term == 'b_mult':
+                test_df = test_df[test_df['b_mult2']-test_df['b_mult1'] == record_dict['b_mult2']-record_dict['b_mult1']]
+            elif term == 'c_mult':
+                test_df = test_df[test_df['c_mult2']-test_df['c_mult1'] == record_dict['c_mult2']-record_dict['c_mult1']]
+            
+            # Compare non-NaN terms
+            elif term in record_dict and pd.notnull(record_dict[term]):
+                test_df = test_df[test_df[term] == record_dict[term]]
+            
+            # Return True if no matching records remain
+            if len(test_df) == 0:
+                return test_df
+
+        # Compare float terms
+        for fterm in fterms:
+            # Compare non-NaN terms
+            if fterm in record_dict and pd.notnull(record_dict[fterm]):
+                test_df = test_df[np.isclose(test_df[fterm],
+                                  record_dict[fterm])]
+       
+            # Return True if no matching records remain
+            if len(test_df) == 0:
+                return test_df
+        
+        # Return False indicating that there are matches
+        return test_df
