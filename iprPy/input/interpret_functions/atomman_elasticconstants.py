f877c8313ee5def4987d09ff8b6633bf8f1145fe
Lucas Hale <lucas.hale@nist.gov>
2018-11-06 13:35:00 -0500

    Renamed elasticconstants_model to elasticconstants_file for consistency

--- iprPy/input/interpret_functions/atomman_elasticconstants.py
+++ iprPy/input/interpret_functions/atomman_elasticconstants.py
@@ -20,15 +20,15 @@ def atomman_elasticconstants(input_dict, build=True, **kwargs):
     The input_dict keys used by this function (which can be renamed using the
     function's keyword arguments):
     
-    - **'elasticconstants_model'** a record containing elastic constants to
+    - **'elasticconstants_file'** a record containing elastic constants to
       load.
     - **'elasticconstants_content'** alternate file or content to load instead
-      of specified elasticconstants_model.  This is used by prepare functions.
+      of specified elasticconstants_file.  This is used by prepare functions.
     - **'C11', 'C12', ..., 'C66'** individually specified elastic constant
       terms.
     - **'C'** atomman.ElasticConstants object.
     - **'load_file'** the system load file, which is searched for elastic
-      constants if neither elasticconstants_model nor Cij terms are specified.
+      constants if neither elasticconstants_file nor Cij terms are specified.
     - **'load_content'** alternate file or content to load instead of
       the specified load_file.
     - **'pressure_unit'** default unit of pressure to use for reading in
@@ -41,8 +41,8 @@ def atomman_elasticconstants(input_dict, build=True, **kwargs):
     build : bool
         If False, parameters will be interpreted, but objects won't be built
         from them (Default is True).
-    elasticconstants_model : str
-        Replacement parameter key name for 'elasticconstants_model'.
+    elasticconstants_file : str
+        Replacement parameter key name for 'elasticconstants_file'.
     elasticconstants_content : str
         Replacement parameter key name for 'elasticconstants_content'.
     Ckey : str
@@ -59,7 +59,7 @@ def atomman_elasticconstants(input_dict, build=True, **kwargs):
     """
     
     # Set default keynames
-    keynames = ['elasticconstants_model', 'elasticconstants_content', 'C',
+    keynames = ['elasticconstants_file', 'elasticconstants_content', 'C',
                 'pressure_unit', 'load_file', 'load_content']
     for keyname in keynames:
         kwargs[keyname] = kwargs.get(keyname, keyname)
@@ -67,7 +67,7 @@ def atomman_elasticconstants(input_dict, build=True, **kwargs):
     # Extract input values and assign default values
     C = None
     Ckey = kwargs.get('Ckey', 'C')
-    elasticconstants_model = input_dict.get(kwargs['elasticconstants_model'], None)
+    elasticconstants_file = input_dict.get(kwargs['elasticconstants_file'], None)
     elasticconstants_content = input_dict.get(kwargs['elasticconstants_content'], None)
     load_file = input_dict.get(kwargs['load_file'], None)
     load_content = input_dict.get(kwargs['load_content'], None)
@@ -75,7 +75,7 @@ def atomman_elasticconstants(input_dict, build=True, **kwargs):
     
     # Replace model with content if given
     if elasticconstants_content is not None:
-        elasticconstants_model = elasticconstants_content
+        elasticconstants_file = elasticconstants_content
     
     # Pull out any single elastic constant terms
     Cdict = {}
@@ -86,13 +86,13 @@ def atomman_elasticconstants(input_dict, build=True, **kwargs):
             Cdict['C'+keytail] = value(input_dict, key, default_unit=pressure_unit)
     
     # If model is given
-    if elasticconstants_model is not None:
+    if elasticconstants_file is not None:
         assert len(Cdict) == 0, (keyhead + 'ij values and '
-                                 + kwargs['elasticconstants_model']
+                                 + kwargs['elasticconstants_file']
                                  + ' cannot both be specified.')
         
         if build is True:
-            C = am.ElasticConstants(model=DM(elasticconstants_model))
+            C = am.ElasticConstants(model=DM(elasticconstants_file))
     
     # Else if individual Cij terms are given
     elif len(Cdict) > 0:
