f666e589d8d255c53aecf5f4d0fa5cecb455433a
Lucas Hale <lucas.hale@nist.gov>
2019-04-25 09:56:46 -0400

    Updated composition assignment as now part of atomman

--- iprPy/analysis/__init__.py
+++ iprPy/analysis/__init__.py
@@ -1,2 +1,2 @@
-from .assign_composition import assign_composition, composition_str
+from .assign_composition import assign_composition
 from .assign_currentIPR import assign_currentIPR, current_ipr_potential_ids
--- iprPy/analysis/assign_composition.py
+++ iprPy/analysis/assign_composition.py
@@ -27,13 +27,13 @@ def assign_composition(df, database, lib_directory=None):
     
     # Identify compositions
     compositions = []
-    for i, series in df.iterrows():
+    for series in df.itertuples():
         
         # Use ucell system if available (crystal_space_group)
         if 'ucell' in series:
-            if series.ucell.symbols[0] is not None:
-                counts = np.unique(series.ucell.atoms.atype, return_counts=True)[1]
-                compositions.append(composition_str(series.ucell.symbols, counts))
+            composition = series.ucell.composition
+            if composition is not None:
+                compositions.append(composition)
             else:
                 compositions.append(np.nan)
         
@@ -42,7 +42,7 @@ def assign_composition(df, database, lib_directory=None):
             
             # If family is a prototype
             if series.family in counts:
-                compositions.append(composition_str(series.symbols, counts[series.family]))
+                compositions.append(am.tools.compositionstr(series.symbols, counts[series.family]))
             
             # If family is a ref
             else:
@@ -54,53 +54,7 @@ def assign_composition(df, database, lib_directory=None):
                     compositions.append(np.nan)
                 else:
                     count = np.unique(ucell.atoms.atype, return_counts=True)[1]
-                    compositions.append(composition_str(ucell.symbols, count))
+                    compositions.append(am.tools.compositionstr(ucell.symbols, count))
         else:
             compositions.append(np.nan)
     df['composition'] = compositions
-
-def composition_str(symbols, counts):
-    """
-    Generates a composition string for a unit cell.
-    
-    Parameters
-    ----------
-    symbols : list
-        All element model symbols.
-    count : list
-        How many unique sites are occupied by each symbol.
-    
-    Returns
-    -------
-    str
-        The composition string.
-    """
-    primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
-    
-    sym_dict = {}
-    for i in range(len(symbols)):
-        sym_dict[symbols[i]] = counts[i]
-    
-    for prime in primes:
-        if max(sym_dict.values()) < prime:
-            break
-        
-        while True:
-            breaktime = False
-            for value in sym_dict.values():
-                if value % prime != 0:
-                    breaktime = True
-                    break
-            if breaktime:
-                break
-            for key in sym_dict:
-                sym_dict[key] /= prime
-    
-    composition =''
-    for key in sorted(sym_dict):
-        if sym_dict[key] > 0:
-            composition += key
-            if sym_dict[key] != 1:
-                composition += str(int(sym_dict[key]))
-    
-    return composition
