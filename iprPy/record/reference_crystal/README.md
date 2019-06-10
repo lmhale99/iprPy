79c64fb7154c0614526aa18bd5545bcd93208d08
Lucas Hale <lucas.hale@nist.gov>
2019-06-10 09:48:52 -0400

    Added reference_crystal record style

---
+++ iprPy/record/reference_crystal/README.md
@@ -0,0 +1,15 @@
+# reference_crystal Record style
+
+**Lucas M. Hale**, [lucas.hale@nist.gov](mailto:lucas.hale@nist.gov?Subject=ipr-demo), *Materials Science and Engineering Division, NIST*.
+
+**Chandler A. Becker**, [chandler.becker@nist.gov](mailto:chandler.becker@nist.gov?Subject=ipr-demo), *Office of Data and Informatics, NIST*.
+
+**Zachary T. Trautt**, [zachary.trautt@nist.gov](mailto:zachary.trautt@nist.gov?Subject=ipr-demo), *Materials Measurement Science Division, NIST*.
+
+Version: 2019-6-6
+
+[Disclaimers](http://www.nist.gov/public_affairs/disclaimer.cfm)
+
+## Introduction
+
+The reference_crystal Record style defines a crystal structure as obtained from an external database.
---
+++ iprPy/record/reference_crystal/ReferenceCrystal.py
@@ -0,0 +1,81 @@
+# Standard Python libraries
+from __future__ import (absolute_import, print_function,
+                        division, unicode_literals)
+from pathlib import Path
+
+# http://www.numpy.org/
+import numpy as np
+
+# https://github.com/usnistgov/DataModelDict
+from DataModelDict import DataModelDict as DM
+
+# https://github.com/usnistgov/atomman
+import atomman as am
+
+# iprPy imports
+from .. import Record
+
+class ReferenceCrystal(Record):
+    
+    @property
+    def contentroot(self):
+        """str: The root element of the content"""
+        return 'reference-crystal'
+    
+    @property
+    def compare_terms(self):
+        """
+        list of str: The default terms used by isnew() for comparisons.
+        """
+        return [
+               'id',
+               ]    
+    
+    def todict(self, full=True, flat=False):
+        """
+        Converts the structured content to a simpler dictionary.
+        
+        Parameters
+        ----------
+        full : bool, optional
+            Flag used by the calculation records.  A True value will include
+            terms for both the calculation's input and results, while a value
+            of False will only include input terms (Default is True).
+        flat : bool, optional
+            Flag affecting the format of the dictionary terms.  If True, the
+            dictionary terms are limited to having only str, int, and float
+            values, which is useful for comparisons.  If False, the term
+            values can be of any data type, which is convenient for analysis.
+            (Default is False).
+            
+        Returns
+        -------
+        dict
+            A dictionary representation of the record's content.
+        """
+        
+        crystal = self.content[self.contentroot]
+        params = {}
+        params['key'] = crystal['key']
+        params['id'] = crystal['id']
+        
+        params['sourcename'] = crystal['source']['name']
+        params['sourcelink'] = crystal['source']['link']
+        
+        ucell = am.load('system_model', self.content, key='atomic-system')
+        params['composition'] = ucell.composition
+        params['natypes'] = ucell.natypes
+        
+        if flat is True:
+            params['symbols'] = list(ucell.symbols)
+            params['a'] = ucell.box.a
+            params['b'] = ucell.box.b
+            params['c'] = ucell.box.c
+            params['alpha'] = ucell.box.alpha
+            params['beta'] = ucell.box.beta
+            params['gamma'] = ucell.box.gamma
+            params['natoms'] = ucell.natoms
+        else:
+            params['ucell'] = ucell
+        
+        return params
