d571a4a4737d9e839c4422eee9ff22576258f897
Lucas Hale <lucas.hale@nist.gov>
2021-08-02 16:28:11 -0400

    workflow module removed.  Orphaned functions moved to analysis for holding

--- iprPy/workflow/__init__.py
+++
@@ -1,8 +0,0 @@
-
-#from . import prepare
-#from . import process
-#from .multi_runners import multi_runners
-from .fix_lammps_versions import fix_lammps_versions
-#from .test_lammps_versions import test_lammps_versions
-from .master_prepare import master_prepare
-
