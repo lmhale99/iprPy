f5f184b45a7049f191169beb036386e108a82fc8
Lucas Hale <lucas.hale@nist.gov>
2022-03-03 17:47:22 -0500

    import comments

--- iprPy/tools/__init__.py
+++ iprPy/tools/__init__.py
@@ -1,9 +1,16 @@
 # coding: utf-8
+
+# https://github.com/usnistgov/yabadaba
 from yabadaba.tools import aslist, iaslist, screen_input
+
+# https://github.com/usnistgov/atomman
 from atomman.tools import filltemplate
+
+# local imports
 from .dynamic_import import dynamic_import
 from .read_calc_file import read_calc_file
 from .dict_insert import dict_insert
 
-__all__ = sorted(['aslist', 'iaslist', 'filltemplate', 'screen_input',
-                  'dynamic_import', 'dict_insert', 'read_calc_file'])
+__all__ = ['aslist', 'iaslist', 'filltemplate', 'screen_input',
+           'dynamic_import', 'dict_insert', 'read_calc_file']
+__all__.sort()
